7fe88e215bfa452dbd7fc081961ed35eba70c70a
peter_alfie <alfie1104@gmail.com>
2019-10-02 08:01:36 +0900

    aasfsae

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from "react";
+import reducer, {
+  initialState,
+  ADD,
+  DEL,
+  COMPLETE,
+  UNCOMPLETE
+} from "./reducer";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const [newToDo, setNewToDo] = useState("");
+
+  const onSubmit = e => {
+    e.preventDefault();
+    dispatch({ type: ADD, payload: newToDo });
+    setNewToDo("");
+  };
+
+  const onChange = e => {
+    const {
+      target: { value }
+    } = e;
+
+    setNewToDo(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Add To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={newToDo}
+          type="text"
+          placeholder="Write to do"
+          onChange={onChange}
+        />
+      </form>
+      <ul>
+        <h2>To Dos</h2>
+        {state.toDos.map((toDo, index) => (
+          <li key={toDo.id}>
+            <span>{toDo.text}</span>
+            <span onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+              X
+            </span>
+            <span
+              onClick={() => dispatch({ type: COMPLETE, payload: toDo.id })}
+            >
+              V
+            </span>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {state.completed.length !== 0 && (
+          <>
+            <h2>Completed</h2>
+            {state.completed.map((toDo, index) => (
+              <li key={toDo.id}>
+                <span>{toDo.text}</span>
+                <span onClick={() => dispatch({ type: DEL, payload: toDo.id })}>
+                  X
+                </span>
+                <span
+                  onClick={() =>
+                    dispatch({ type: UNCOMPLETE, payload: toDo.id })
+                  }
+                >
+                  ○
+                </span>
+              </li>
+            ))}
+          </>
+        )}
+      </ul>
+    </>
   );
 }
 
